98cafdf228760daa843b9f6840b98188bacaf048
Julie <jem.baret@gmail.com>
2023-10-27 16:06:59 +0200

    add basic about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function About() {
+	return (
+		<>
+			<div className="text-6xl font-bold text-slate-600">About Us</div>
+			<hr className="bg-slate-400 h-1 w-full my-4" />
+			<p>
+				Women in View is a project about gender bias
+			</p>
+			<p>
+				Our goal is to hightlight the lack of female representation (and well-written female character) in the cinema industry
+			</p>
+		</>
+	);
+}
