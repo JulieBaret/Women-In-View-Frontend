4c6ca184f68cd5ac7ca439688071fb33361dfd41
Julie <jem.baret@gmail.com>
2024-01-12 14:54:39 +0100

    feat: add SearchBar component and call it in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
+import SearchBar from '../components/SearchBar';
 
 const Home = () => {
+    const [searchValue, setSearchValue] = useState("");
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        console.log(searchValue);
+    }
+
     return (
-        <div className='w-5/6 flex flex-col gap-6 p-20 self-center'>
-            <form>
-                <div className="relative">
-                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                    </div>
-                    <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-primary focus:border-primary" placeholder="Search for movies..." />
-                    <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2">Search</button>
-                </div>
-            </form>
+        <div className='w-5/6 flex flex-col px-20 py-10 self-center'>
+            <SearchBar onSubmit={handleSearch} onChange={(e) => setSearchValue(e.target.value)} value={searchValue} label="Search" placeholder="Search for movies..." />
         </div>
     );
 };
