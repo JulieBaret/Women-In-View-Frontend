854cd81e0149595bbd4f0bbe72ac3ead857b529a
Julie <jem.baret@gmail.com>
2024-01-26 10:58:33 +0100

    style: improve banners

--- src/components/SuccessBanner.tsx
+++ src/components/SuccessBanner.tsx
@@ -8,7 +8,23 @@ type Props = {
 const SuccessBanner = ({ isSuccess, success }: Props) => {
     return (
         <>
-        {isSuccess && <p className='text-green-700 bg-green-100 w-fit px-2 py-1 rounded'>{success}</p>}
+        {isSuccess && <div
+                className="flex p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50"
+                role="alert">
+                <svg
+                    aria-hidden="true"
+                    className="flex-shrink-0 inline w-5 h-5 mr-3"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        fillRule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                        clipRule="evenodd"></path>
+                </svg>
+                <span className="sr-only">Info</span>
+                <div>{success}</div>
+            </div>}
         </>
     );
 };
