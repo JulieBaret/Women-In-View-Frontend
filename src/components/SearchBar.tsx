88f73498734f252320a1f8aa6921b29c02ae10dd
Julie <jem.baret@gmail.com>
2024-02-23 15:53:47 +0100

    style: thinner searchbar for harmony

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -13,12 +13,11 @@ const SearchBar = ({ onSubmit, onChange, value, label, placeholder }: Props) => 
         <form onSubmit={onSubmit}>
             <div className="relative">
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                    <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                    </svg>
                 </div>
-                <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-primary focus:border-primary" placeholder={placeholder} onChange={onChange} value={value} />
-                <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-primary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2">{label}</button>
+                <input type="search" id="default-search" className="block w-56 h-10 px-4 text-sm text-light border border-transparent rounded-full bg-gray-50 bg-opacity-30 focus:ring-primary focus:border-primary placeholder-light" placeholder={placeholder} onChange={onChange} value={value} />
+                <button type="submit" className="text-light absolute end-2 bottom-1.5 bg-primary hover:bg-primary-hovered focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-2 py-2"><svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg></button>
             </div>
         </form>
     );
