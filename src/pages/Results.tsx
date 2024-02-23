efeb22d3d8773f2c002f4b96fb0702a551303b46
Julie <jem.baret@gmail.com>
2024-02-23 15:15:23 +0100

    feat: results component calling searchResults allowing history navigation

---
+++ src/pages/Results.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import ErrorBanner from '../components/ErrorBanner';
+import Heading from '../components/Heading';
+import SearchResults from '../components/SearchResults';
+
+const Results = () => {
+    const params = useParams();
+    const { query } = params;
+    const [dataFromTmdb, setDataFromTmdb] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+    const [errorFromTmdb, setErrorFromTmdb] = useState("");
+    
+    useEffect(() => {
+        setIsPending(true);
+
+        // Fetch options
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`
+            }
+        };
+
+        // Fetch from Tmdb
+        fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, options)
+            .then(response => response.json())
+            .then((data) => {
+                setDataFromTmdb(data);
+            })
+            .catch((err) => {
+                console.error(err);
+                setErrorFromTmdb(err);
+            });
+        
+
+    }, [query])
+
+    return (
+        <main className="flex justify-center flex-col">
+            <Heading variant='large'>Results for «{query}»</Heading>
+            <div className="mt-8 p-2">
+                {errorFromTmdb && <ErrorBanner isError={Boolean(errorFromTmdb)} error="It's been a problem while fetching data" />}
+                {!errorFromTmdb && dataFromTmdb && <SearchResults dataFromTmdb={dataFromTmdb} isPending={isPending} setIsPending={setIsPending} />}
+            </div>
+        </main>
+    );
+};
+
+export default Results;
