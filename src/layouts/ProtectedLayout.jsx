71029dfb1f235fa7be7b14fd40f2ca03cedaf213
Julie <jem.baret@gmail.com>
2024-02-24 21:13:22 +0100

    fix: responsive navbar

--- src/layouts/ProtectedLayout.jsx
+++ src/layouts/ProtectedLayout.jsx
@@ -5,7 +5,9 @@ import Axios from 'axios';
 import { useAuth } from '../contexts/AuthContext';
 import { useState } from 'react';
 import ActiveNavLink from '../components/ActiveNavLink';
-import { Avatar, Dropdown } from 'flowbite-react';
+import { Dropdown } from 'flowbite-react';
+import { HiCog, HiLogout, HiViewGrid } from 'react-icons/hi';
+
 import FullScreenLoading from '../components/FullScreenLoading';
 import SearchBar from '../components/SearchBar';
 
@@ -67,14 +69,11 @@ export default function ProtectedLayout() {
 						<NavLink
 							to="/home"
 							className="self-center text-xl font-semibold whitespace-nowrap text-white">
-							<span className='text-3xl font-bold text-light'>Women in View</span>
+							<span className='text-3xl font-bold text-light hidden sm:block'>Women in View</span>
 						</NavLink>
 					</div>
-					<div>
-						<SearchBar onSubmit={handleSearch} onChange={(e) => setSearchValue(e.target.value)} value={searchValue} label="Search" placeholder="Search for a movie title..." />
-					</div>
-					<div className="flex md:gap-10">
-						<button type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-300 rounded-lg md:hidden hover:text-light focus:outline-none order-last" onClick={handleOpenMenu}>
+					<div className="flex lg:gap-10">
+						<button type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-300 rounded-lg lg:hidden hover:text-light focus:outline-none order-last" onClick={handleOpenMenu}>
 							{isMenuOpen ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
 								<path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
 							</svg>
@@ -82,16 +81,13 @@ export default function ProtectedLayout() {
 									<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
 								</svg>}
 						</button>
-						<div className="hidden w-full md:block md:w-auto">
-							<ul className="flex flex-col md:flex-row text-center text-gray-300 md:mb-0 mb-4">
+						<div className="hidden w-full lg:block lg:w-auto">
+							<ul className="flex flex-col lg:flex-row text-center text-gray-300 lg:mb-0 mb-4">
 								<li>
-									<ActiveNavLink label="Movies" location="/home" />
+									<ActiveNavLink label="Home" location="/home" />
 								</li>
 								<li>
-									<ActiveNavLink label="Reviews" location="/reviews" />
-								</li>
-								<li>
-									<ActiveNavLink label="The Bechdel Test" location="/about" />
+									<ActiveNavLink label="Last reviews" location="/" />
 								</li>
 								{/* <li>
 								<div className="flex w-[110px]">
@@ -105,7 +101,9 @@ export default function ProtectedLayout() {
 							</li> */}
 							</ul>
 						</div>
+						<SearchBar onSubmit={handleSearch} onChange={(e) => setSearchValue(e.target.value)} value={searchValue} label="Search" placeholder="Search for a movie..." />
 						<div className='flex gap-2 items-center'>
+							{/* Dropdown profile menu */}
 							<Dropdown
 								arrowIcon={false}
 								inline
@@ -116,47 +114,31 @@ export default function ProtectedLayout() {
 										{/* <Avatar className='rounded-full' alt="User settings" img="/profile.png" rounded /> */}
 									</div>
 								}
-								color="light"
 							>
 								<Dropdown.Header>
 									<span className="block text-sm">{user.name}</span>
 									<span className="block truncate text-sm font-medium">{user.email}</span>
 								</Dropdown.Header>
-								<Dropdown.Item>
-									<NavLink to="/profile">
-										<div className="flex gap-1 items-center">
-											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-												<path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
-												<path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-											</svg>
-											<span>Edit my profile</span>
-										</div></NavLink>
-								</Dropdown.Item>
+								<Dropdown.Item icon={HiViewGrid}><NavLink to="/reviews">My reviews</NavLink></Dropdown.Item>
+								<Dropdown.Item icon={HiCog}><NavLink to="/profile">Profile settings</NavLink></Dropdown.Item>
 								<Dropdown.Divider />
-								<Dropdown.Item>
-									<div className='flex gap-1'
-											onClick={handleLogout}>
-										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-											<path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
-										</svg>
-										<span>Logout</span>
-									</div>
-								</Dropdown.Item>
+								<Dropdown.Item icon={HiLogout} onClick={handleLogout}>Sign out</Dropdown.Item>
 							</Dropdown>
 						</div>
 					</div>
 				</div>
-				{isMenuOpen && <div className="h-auto md:hidden">
-					<ul className="flex flex-col text-center text-gray-300 gap-2" onClick={handleOpenMenu}>
+				{isMenuOpen && <div className="h-auto lg:hidden flex flex-col text-center text-gray-300 mt-2">
+					<ul onClick={handleOpenMenu}>
 						<li>
-							<ActiveNavLink label="Movies" location="/home" />
+							<ActiveNavLink label="Home" location="/home" />
 						</li>
+						
 						<li>
-							<ActiveNavLink label="Reviews" location="/reviews" />
+							<ActiveNavLink label="Last reviews" location="/" />
 						</li>
-						<li>
+						{/* <li>
 							<ActiveNavLink label="The Bechdel Test" location="/about" />
-						</li>
+						</li> */}
 						{/* <li>
 								<div className="flex w-[110px]">
 									<button
