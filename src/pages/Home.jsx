ff7fd76ea3560b291388ea0bf86c855b3b1dece6
Julie <jem.baret@gmail.com>
2024-02-24 23:09:08 +0100

    feat: home page with overview of the project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,60 +1,43 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import Loading from '../components/Loading';
-import SearchBar from '../components/SearchBar';
-import SearchResults from '../components/SearchResults';
-import ErrorBanner from '../components/ErrorBanner';
+import React from 'react';
 import Heading from '../components/Heading';
+import { RiDoubleQuotesL } from "react-icons/ri";
+
 
 const Home = () => {
-    const [value, setValue] = useState("");
-    const [dataFromTmdb, setDataFromTmdb] = useState({});
-    const [isPending, setIsPending] = useState(false);
-    const [errorFromTmdb, setErrorFromTmdb] = useState("");
-    const [lengthError, setLengthError] = useState(false);
-
-    const handleSearch = async (e) => {
-        e.preventDefault();
-        if (value.length < 3) {
-            setLengthError(true);
-            return;
-        }
-        setIsPending(true);
-
-        // Fetch from Tmdb
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`
-            }
-        };
-        fetch(`https://api.themoviedb.org/3/search/movie?query=${value}&include_adult=false&language=en-US&page=1`, options)
-            .then(response => response.json())
-            .then((data) => {
-                console.log(data)
-                setDataFromTmdb(data)
-            })
-            .catch((err) => {
-                console.error(err);
-                setErrorFromTmdb(error);
-            });
-    };
-
 
     return (
-        <div>
-            <img className='w-full object-cover h-48' alt='Bechdel comic extract' src='./banner.png'/>
-            <main className="flex justify-center flex-col items-center">
-                <div className='w-full flex flex-col gap-6'>
-                    <div className="w-full md:w-4/6 self-center">
-                        <SearchBar onSubmit={handleSearch} onChange={(e) => setValue(e.target.value)} value={value} label="Search" placeholder="Search for a movie title..." />
-                    </div>
-                    {lengthError && <ErrorBanner isError={lengthError} error="You must type at least 3 characters" />}
-                    {errorFromTmdb && <ErrorBanner isError={errorFromTmdb} error="It's been a problem while fetching data" />}
-                    {!lengthError && !errorFromTmdb && dataFromTmdb && <SearchResults dataFromTmdb={dataFromTmdb} isPending={isPending} setIsPending={setIsPending} />}
-                </div>
-            </main>
+        <div className='flex flex-col items-center gap-10'>
+            <img className='w-full object-cover h-48' alt='Bechdel comic extract' src='./banner.png' />
+            <div className=" md:w-4/6">
+                <figure className="max-w-screen-md mx-auto px-10 text-center">
+                    <svg className="w-10 h-10 mx-auto mb-3 text-grey " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+                        <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+                    </svg>
+                    <blockquote>
+                        <p className='text-2xl md:text-4xl md:font-extrabold font-bold '><span className='italic'>I only go to a movie if it satisfies <span className='underline'>three basic requirements</span>. One, it has to have at least two women in it who, two, talk to each other about, three, something besides a man.</span></p>
+                    </blockquote>
+                    <figcaption className="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse px-10 md:px-0">
+                        <img className="w-10 rounded-full" src="https://meme-pas-mal.fr/wp-content/uploads/2016/11/portraits-auteurs_alison.jpg" alt="profile picture" />
+                        <div className="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500">
+                            <cite className="pe-3 font-medium text-dark">Comic book author Alison Bechdel</cite>
+                            <cite className="px-3 text-sm text-grey">"Dykes to Watch Out For"</cite>
+                        </div>
+                    </figcaption>
+                </figure>
+                <main className='flex flex-col gap-4'>
+                    <Heading variant="medium">It all started with a joke...</Heading>
+                    <article className='flex flex-col gap-4 text-lg text-justify'>
+                    <p>A joke written by the American and openly gay cartoonist Alison Bechdel, in her comic book named "Dykes to Watch Out for", before it was emphasized by Liz Wallace in an essay about women and cinema.</p>
+                    <p>Originally conceived as a humorous observation on the portrayal of women in films, the "Bechdel-Wallace Test" (often called the "Bechdel Test") has since evolved into a powerful tool for dissecting gender bias in media. Despite its straightforwardness, passing the test remains a significant challenge for many films and TV shows, revealing the persistent tendency to sideline female characters and prioritize male-centric narratives.</p>
+                    <figure className='py-2'>
+                        <img alt="The Rules strip from Dykes to Watch Out For" src="https://dykestowatchoutfor.com/wp-content/uploads/2014/05/The-Rule-cleaned-up.jpg"/>
+                        <figcaption className="flex items-center justify-center space-x-3 rtl:space-x-reverse"><cite className='text-xs text-grey'>"The Rules" strip from "Dykes to Watch Out for" published in 1985</cite></figcaption>
+                    </figure>
+                    <p>By highlighting the absence of meaningful interactions between female characters and exposing the pervasive gender biases embedded in storytelling, the Bechdel-Wallace Test has sparked vital conversations about representation, diversity, and gender equality in the media landscape. Even though it does determines a film's quality or feminist credentials, it serves as a starting point for deeper analysis and discussion about the complexities of gender representation and intersectionality in media.</p>
+                    <p className='text-2xl font-bold'>This collaborative website allows users to see which films pass or fail the Bechdel test. Everyone is invited to participate by reviewing movies with the Bechdel-Wallace Test.</p>
+                    </article>
+                </main>
+            </div>
         </div>
     );
 };
