c651c414120b720c3c209dc3d9d2e2d420de411c
Julie <jem.baret@gmail.com>
2024-03-01 21:16:42 +0100

    fix: env api value type

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
     readonly VITE_APP_TITLE: string
     readonly VITE_TMDB_API_KEY: string
     readonly VITE_TMDB_TOKEN: string
+    readonly VITE_API_URL: string
 }
 
 interface ImportMeta {
