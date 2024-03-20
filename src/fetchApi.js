ec5394f50e391a1dbc9b591910f712394d576fcd
Julie <jem.baret@gmail.com>
2024-03-20 22:03:31 +0100

    refacto: comment fetchApi for clarity

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,9 +1,9 @@
 import Axios from 'axios';
 
+// is only used for sign in and sign up, while there is no need to pass a bearer token
 const fetchApi = Axios.create({
 	baseURL: "http://localhost:80/api",
 	withCredentials: true,
-	//cookies will be sent along with the requests, allowing authenticated requests
 	headers: {
 		"Content-Type": "application/json",
 		"Accept": "application/json",
