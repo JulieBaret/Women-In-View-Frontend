350d54ef149ea661848bdda9fb027149737be64a
Julie <jem.baret@gmail.com>
2024-03-20 21:38:42 +0100

    fix: update d.ts files and add a tsconfig.json

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,8 @@
 /// <reference types="vite/client" />
 
+import 'vite/client'
+
 interface ImportMetaEnv {
-    readonly VITE_APP_TITLE: string
-    readonly VITE_TMDB_API_KEY: string
-    readonly VITE_TMDB_TOKEN: string
     readonly VITE_API_URL: string
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-/// <reference types="vite/client" />
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {}
+  }
 })
-
-interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string
-  readonly VITE_TMDB_API_KEY: string
-  readonly VITE_TMDB_TOKEN: string
-  readonly VITE_API_URL: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
