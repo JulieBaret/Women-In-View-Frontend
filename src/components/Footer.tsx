dbae9959bdecfb7319fff6b5dcc69ad92c80043f
Julie <jem.baret@gmail.com>
2024-03-21 19:02:03 +0100

    style: remove top margin so footer content is correctly centered

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="bg-gray-800 text-light font-thin text-center w-full">
-            <div className="mx-auto mt-20 p-14 md:p-24 w-[90vw] md:w-[75vw]">
-            <p><span className='italic'>Women in View</span> is a school project made by Marielle Koffi and Julie Baret, software developers.</p>
-            <p>They both students at Ada Tech School, a training institution they joined within the Chimamanda Ngozie Adichie promotion, in 2022.
-            </p>
-            <p className='mt-8'>It is based on the inspirational idea of their friend, Iris Rossiquet, video editor in Paris.
-            </p>
+            <div className="mx-auto p-14 md:p-24 w-[90vw] md:w-[75vw]">
+                <p><span className='italic'>Women in View</span> is a school project made by Marielle Koffi and Julie Baret, software developers.</p>
+                <p>They both students at Ada Tech School, a training institution they joined within the Chimamanda Ngozie Adichie promotion, in 2022.
+                </p>
+                <p className='mt-8'>It is based on the inspirational idea of their friend, Iris Rossiquet, video editor in Paris.
+                </p>
             </div>
         </div>
     );
