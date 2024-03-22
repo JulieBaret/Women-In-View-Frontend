c1d13e5c1e1e574c0db21641312f5384b68cdff1
Julie <jem.baret@gmail.com>
2024-03-22 17:43:52 +0100

    import env variable to fetch api to prepare deployment

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 
 // is only used for sign in and sign up, while there is no need to pass a bearer token
 const fetchApi = Axios.create({
-	baseURL: "http://localhost:80/api",
+	baseURL: import.meta.env.VITE_API_URL,
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
