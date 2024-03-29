c78d335103c23fc9825d9d5eb3b45c4c0512182c
Julie <jem.baret@gmail.com>
2024-03-29 16:01:04 +0100

    style: add fade animation to hero and infocard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,17 +10,17 @@ const Home = () => {
         <div className='flex flex-col items-center'>
             <div className="flex flex-col md:flex-row md:h-[93vh] md:justify-evenly items-evenly md:items-center mt-10 md:mt-0">
                 <div className='flex flex-col gap-6 w-[90vw] md:w-1/3'>
-                    <h1 className='text-4xl lg:text-5xl 2xl:text-6xl font-black font-fraunces'>The collaborative initiative
+                    <h1 className='fadeInUp-animation text-4xl lg:text-5xl 2xl:text-6xl font-black font-fraunces'>The collaborative initiative
                         that puts female characters
                         in perspective</h1>
-                    <h2 className='text-lg lg:text-xl 2xl:text-2xl font-medium leading-tight'>Find out which films pass or fail
+                    <h2 className='fadeInUp-animation text-lg lg:text-xl 2xl:text-2xl font-medium leading-tight'>Find out which films pass or fail
                         the <a className="underline decoration-dotted underline-offset-4 decoration-primary" href="#bechdelTest">Bechdel Test</a> and test movies
                         yourself!</h2>
-                    <div className='block w-fit bg-primary rounded-full'>
+                    <div className='fadeInUp-animation block w-fit bg-primary rounded-full'>
                         <SearchInput />
                     </div>
                 </div>
-                <img className='w-[90vw] md:w-[50vw] xl:w-[40vw] py-10' alt="Sketches of female characters from Thelma and Louise, Portrait of a Lady on Fire, Woman King and The Purple Color" src="hero.webp" />
+                <img className='fadeInUp-animation w-[90vw] md:w-[50vw] xl:w-[40vw] py-10' alt="Sketches of female characters from Thelma and Louise, Portrait of a Lady on Fire, Woman King and The Purple Color" src="hero.webp" />
             </div>
             <div className='bg-secondary sticky opacity-50 h-16 flex items-center pb-1 w-full'><p className='text-3xl text-light font-black font-fraunces overflow-hidden whitespace-nowrap'>men in view. women in view. women in view. women in view. women in view. women in view. women in view. women in view. women in view. women in view. women in view. women in view.</p></div>
             <main id="bechdelTest" className='flex flex-col gap-4'>
