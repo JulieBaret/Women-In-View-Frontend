ee1fc648bdb0e85d638ff5b5a5f2b39b001d32f2
Julie <jem.baret@gmail.com>
2024-03-29 16:49:38 +0100

    chore: use lady loading for all react pages to improve perf

--- src/router.jsx
+++ src/router.jsx
@@ -1,45 +1,109 @@
+import * as React from 'react';
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
-import About from './pages/About';
-import Profile from './pages/Profile';
-import ProtectedLayout from './layouts/ProtectedLayout';
-import GuestLayout from './layouts/GuestLayout';
-import Home from './pages/Home';
-import ForgotPassword from './pages/ForgotPassword';
-import UserReview from './pages/UserReview';
-import Results from './pages/Results';
-import TestedMovies from './pages/TestedMovies';
-import AdminLayout from './layouts/AdminLayout';
-import Users from './pages/Users';
-import Reviews from './pages/Reviews';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
+
+// Components
+import FullScreenLoading from './components/FullScreenLoading';
+
+// Layouts
+const ProtectedLayout = React.lazy(() => import('./layouts/ProtectedLayout'));
+const GuestLayout = React.lazy(() => import('./layouts/GuestLayout'));
+const AdminLayout = React.lazy(() => import('./layouts/AdminLayout'));
+
+// Pages
+const SignIn = React.lazy(() => import('./pages/SignIn'));
+const SignUp = React.lazy(() => import('./pages/SignUp'));
+const Home = React.lazy(() => import('./pages/Home'));
+const About = React.lazy(() => import('./pages/About'));
+const Profile = React.lazy(() => import('./pages/Profile'));
+const ForgotPassword = React.lazy(() => import('./pages/ForgotPassword'));
+const UserReview = React.lazy(() => import('./pages/UserReview'));
+const Results = React.lazy(() => import('./pages/Results'));
+const TestedMovies = React.lazy(() => import('./pages/TestedMovies'));
+const Users = React.lazy(() => import('./pages/Users'));
+const Reviews = React.lazy(() => import('./pages/Reviews'));
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route>
-			<Route path="/" element={<GuestLayout />}>
-				<Route path="/" element={<SignIn />} />
-				<Route path="register" element={<SignUp />} />
-				<Route path="forgot-password" element={<ForgotPassword />} />
+			<Route path="/" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<GuestLayout />
+					</React.Suspense>
+				}>
+				<Route path="/" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<SignIn />
+					</React.Suspense>
+				} />
+				<Route path="register" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<SignUp />
+					</React.Suspense>
+				} />
+				<Route path="forgot-password" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<ForgotPassword />
+					</React.Suspense>
+				} />
 			</Route>
-			<Route path="/" element={<ProtectedLayout />}>
-				<Route path="home" element={<Home />} />
-				<Route path="tested-movies" element={<TestedMovies />} />
-				<Route path="about" element={<About />} />
+			<Route path="/" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<ProtectedLayout />
+					</React.Suspense>
+				}>
+				<Route path="/home" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<Home />
+					</React.Suspense>
+				} />
+				<Route path="tested-movies" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<TestedMovies />
+					</React.Suspense>
+				} />
+				<Route path="about" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<About />
+					</React.Suspense>
+				} />
 				<Route path="user">
 					<Route path="profile">
-						<Route path=":id" element={<Profile />} />
+						<Route path=":id" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<Profile />
+					</React.Suspense>
+				} />
 					</Route>
 					<Route path="reviews">
-						<Route path=":userId" element={<UserReview />} />
+						<Route path=":userId" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<UserReview />
+					</React.Suspense>
+				} />
 					</Route>
 				</Route>
 				<Route path="search">
-					<Route path=":query" element={<Results />} />
+					<Route path=":query" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<Results />
+					</React.Suspense>
+				} />
 				</Route>
-				<Route path="admin" element={<AdminLayout />}>
-					<Route path="users" element={<Users />} />
-					<Route path="reviews" element={<Reviews />} />
+				<Route path="admin" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<AdminLayout />
+					</React.Suspense>
+				}>
+					<Route path="users" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<Users />
+					</React.Suspense>
+				} />
+					<Route path="reviews" element={
+					<React.Suspense fallback={<FullScreenLoading />}>
+						<Reviews />
+					</React.Suspense>
+				} />
 				</Route>
 			</Route>
 		</Route>
