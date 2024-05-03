bb70ece6f9cafcb58ad0089ee1048b229ff4e626
Julie <jem.baret@gmail.com>
2024-05-03 17:06:31 +0200

    style: fix fullscreenloading label padding on small screens

--- src/components/FullScreenLoading.tsx
+++ src/components/FullScreenLoading.tsx
@@ -8,7 +8,7 @@ type Props={
 const FullScreenLoading = ({label}: Props) => {
     return (
         <div className="fixed h-screen w-screen bg-white z-30 flex justify-center items-center">
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center p-10 md:p-0">
                 <div className="loadingio-spinner-wedges-nq4q5u6dq7r"><div className="ldio-x2uulkbinbj">
                     <div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div></div>
                 </div></div>
