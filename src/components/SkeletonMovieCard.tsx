002df1c2cfea8e6124e3bf77d2b8f650fde3decc
Julie <jem.baret@gmail.com>
2024-05-03 16:05:33 +0200

    style: improve skeleton movie card shape

--- src/components/SkeletonMovieCard.tsx
+++ src/components/SkeletonMovieCard.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const SkeletonMovieCard = () => {
     return (
-        <div className={'w-64 h-[400px] rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] animate-pulse bg-gray-300'}>
+        <div className="rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] animate-pulse w-[300px]">
+            <div className="relative bg-gray-300 rounded-t-lg h-[400px]"></div> {/* Ajustez la hauteur ici */}
+            <div className="px-4 py-4 flex flex-col gap-2 h-20 justify-between"> {/* Ajustez la hauteur ici */}
+                <div className="h-6 bg-gray-300 rounded w-[80%]"></div> {/* Ajustez la largeur ici */}
+                <div className="h-6 bg-gray-200 rounded-full w-16"></div>
+            </div>
         </div>
     );
 };
