750e609a529e344f36931c52b0c6f0cbbde431c3
Julie <jem.baret@gmail.com>
2024-05-10 18:18:24 +0200

    style: fix Home page and footer font size and display

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,10 +9,10 @@ const Footer = () => {
             <div className="w-full lg:w-5/6 py-12">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <NavLink to="/home" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="clap_logo.webp" className="h-8" alt="Women in view logo" />
+                        <img src="/clap_logo.webp" className="h-8" alt="Women in view logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap font-fraunces text-light">Women in view</span>
                     </NavLink>
-                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-light sm:mb-0">
+                    <ul className="flex flex-wrap items-center mb-6 font-medium text-light sm:mb-0">
                         <li>
                             <NavLink to="/about" className="hover:underline me-4 md:me-6">About</NavLink>
                         </li>
@@ -28,7 +28,7 @@ const Footer = () => {
                     </ul>
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-autolg:my-8" />
-                <p className="block text-sm sm:text-center text-light">© {currentYear} <NavLink to="/home">WIV</NavLink>. All Rights Reserved.</p>
+                <p className="block sm:text-center text-light">© {currentYear} <NavLink to="/home">WIV</NavLink>. All Rights Reserved.</p>
             </div>
         </footer>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
         <div className='flex flex-col items-center'>
             <Hero />
             <Banner />
-            <main id="bechdelTest" className='flex flex-col gap-4'>
-                <article className='flex flex-col self-center gap-10 text-lg text-justify lg:w-5/6'>
+            <main className='flex flex-col gap-4'>
+                <article className='flex flex-col self-center gap-10 text-lg sm:text-xl text-justify lg:w-5/6 mb-10'>
                     <Heading variant="large">What is the Bechdel Test?</Heading>
                     <Heading variant="medium">It all started with a joke...</Heading>
                     <p>A joke written by the American and openly gay cartoonist Alison Bechdel in her chronicle (later published as a comic book) <span className='italic'>Dykes to Watch Out For</span>. The American cartoonist, who has been sketching the daily lives of a fictional group of lesbian friends since the 1980s, imagines a conversation between two women. They wonder which movie to go see at the cinema when one of them explains: </p>
@@ -32,20 +32,21 @@ const Home = () => {
                             </div>
                         </figcaption>
                     </figure>
-                    <p>Originally conceived as a humorous observation on the portrayal of women in films inspired by Alison Bechdel's friend Liz Wallace, the test has evolved into a powerful tool for dissecting gender bias in media. Despite its straightforwardness, passing the test remains a significant challenge for many films and TV shows, revealing the persistent tendency to sideline female characters and prioritize male-centric narratives.</p>
                     <div className='flex flex-col md:flex-row items-center md:items-start gap-4 md:gap-0'>
-                        <p className='text-left text-2xl md:text-4xl md:font-extrabold font-bold font-fraunces text-secondary'>50% of movies fail the Bechdel Test</p>
-                        <img className="w-[50vw]" alt="extract of The Rule comic strip" src="comic.webp" />
+                        <p className='text-left text-2xl md:text-4xl md:font-extrabold font-bold font-fraunces text-secondary leading-tight'>50% of movies fail the Bechdel Test</p>
+                        <img className="w-full sm:w-[50vw]" alt="extract of The Rule comic strip" src="comic.webp" />
                     </div>
-                    <p>Despite its basic criteria, FiveThirtyEight.com found out in 2014 that <span className="font-black text-primary underline underline-offset-4">half of the films released between 1970 and 2013 did not pass the Bechdel test</span> (out of approximately 2,000 films analyzed).</p>
-                    <p>By highlighting the absence of meaningful interactions between female characters and exposing the pervasive gender biases embedded in storytelling, the "Bechdel-Wallace Test" has sparked vital conversations about representation, diversity, and gender equality in the media landscape. Even though it does determines a film's quality or feminist credentials, it serves as a starting point for deeper analysis and discussion about the complexities of gender representation and intersectionality in media.</p>
-                    <div className='flex flex-col gap-2'>
+                    <p>Inspired by Alison Bechdel's friend Liz Wallace, the Bechdel Test – also known as Wallace-Bechdel Test – has evolved from a comic strip to a powerful tool for dissecting gender bias in media. In 2014, the media <a className="external-link" href="https://fivethirtyeight.com/features/the-dollar-and-cents-case-against-hollywoods-exclusion-of-women/">FiveThirtyEigh</a> analyzed approximately 2,000 films released between 1970 and 2013 and found out that <span className="font-black text-primary underline underline-offset-4">half of the movies did not pass the Bechdel Test</span> despite of its basic criteria.</p>
+                    <p>Nowadays, passing the Bechdel Test remains a significant challenge for many films and TV shows, revealing the persistent tendency to sideline female characters and prioritize male-centric narratives. By highlighting the absence of meaningful interactions between female characters and exposing the pervasive gender biases embedded in storytelling, it has since sparked vital conversations about representation, diversity, and gender equality in the media landscape.</p>
+                    <p>Even though it does determines a film's quality or feminist credentials, it serves as a starting point for deeper analysis and discussion about the complexities of gender representation and intersectionality in media.</p>
+                    <div className='flex flex-col gap-2 text-left'>
                         <h3 className='text-2xl font-bold font-fraunces'>Ressources:</h3>
                         <ol className='flex flex-col gap-1'>
-                            <li><a className="underline italic decoration-dotted underline-offset-4 decoration-primary" href="https://s26162.pcdn.co/wp-content/uploads/2021/09/bechdeltestcartoon.jpeg" target="_blank">The Rule</a>, the original comic strip from Alison Bechdel's comic book <span className='italic'>Dykes to Watch Out For</span></li>
-                            <li><a className="underline italic decoration-dotted underline-offset-4 decoration-primary" href="https://bechdeltest.com/" target="_blank">Bechdel Test Movie List</a>, a website that maintains a database of thousands of films that have passed or failed the Bechdel Test (and is an inspiration for the <span className='italic'>Women in view</span> initiative)</li>
-                            <li><a className="underline italic decoration-dotted underline-offset-4 decoration-primary" href="https://www.vox.com/2014/7/1/5860562/half-of-2014s-movies-fail-this-basic-test-of-sexism" target="_blank">Half of 2014's movies fail this basic test of sexism</a>, an article from <span className='italic'>Vox</span></li>
-                            <li><a className="underline italic decoration-dotted underline-offset-4 decoration-primary" href="https://www.thenews.com.pk/magazine/instep-today/96432-What-is-the-Bechdel-Test-Many-movies-have-failed-it-and-for-a-good-reason" target="_blank">What is the Bechdel Test? Many movies have failed it, and for a good reason</a>, an article from <span className='italic'>The News</span></li>
+                            <li><a className="external-link" href="https://s26162.pcdn.co/wp-content/uploads/2021/09/bechdeltestcartoon.jpeg" target="_blank">The Rule</a>, the original comic strip from Alison Bechdel's comic book <span className='italic'>Dykes to Watch Out For</span></li>
+                            <li><a className="external-link" href="https://bechdeltest.com/" target="_blank">Bechdel Test Movie List</a>, a website that maintains a database of thousands of films that have passed or failed the Bechdel Test (and is an inspiration for the <span className='italic'>Women in view</span> initiative)</li>
+                            <li><a className="external-link" href="https://fivethirtyeight.com/features/the-dollar-and-cents-case-against-hollywoods-exclusion-of-women/" target="_blank">The Dollar-And-Cents Case Against Hollywood’s Exclusion of Women</a>, the 2014 study led by <span className='italic'>Five Thirty Eight</span></li>
+                            <li><a className="external-link" href="https://www.vox.com/2014/7/1/5860562/half-of-2014s-movies-fail-this-basic-test-of-sexism" target="_blank">Half of 2014's movies fail this basic test of sexism</a>, an article from <span className='italic'>Vox</span></li>
+                            <li><a className="external-link" href="https://www.thenews.com.pk/magazine/instep-today/96432-What-is-the-Bechdel-Test-Many-movies-have-failed-it-and-for-a-good-reason" target="_blank">What is the Bechdel Test? Many movies have failed it, and for a good reason</a>, an article from <span className='italic'>The News</span></li>
                         </ol>
                     </div>
                 </article>
